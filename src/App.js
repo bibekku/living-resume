413577c3fb000d40cb1fd8c86e22ade337001394
Vivek Maurya <vmaurya@cs.stonybrook.edu>
2024-06-21 20:56:51 -0400

    Added react boilerplate

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Ceci n'est pas une webpage</h1>
+        <p>🚧 This page is under construction. Check back later. 🚧</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
